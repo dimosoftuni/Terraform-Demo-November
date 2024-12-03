aca64775f750b920ea05f31880f47bddf86a66b3
Dimo Mitev <dimosoftuni@gmail.com>
2024-12-03 20:35:17 +0200

    add backend resource

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.11.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragedimomite"
+    container_name       = "taskboardstoragedimo"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
